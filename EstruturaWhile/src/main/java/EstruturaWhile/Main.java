531d1d802a9814a9b5d0c5ef21eb7bf9a6cd61e7
Ezequiel-Zyyka <ezequiel.braz.es@gmail.com>
2023-01-27 06:15:08 -0300

    Adicionei exercicio de loop sem erro de nextLine

---
+++ EstruturaWhile/src/main/java/EstruturaWhile/Main.java
@@ -0,0 +1,31 @@
+package EstruturaWhile;
+
+import java.util.Scanner;
+/**
+ *
+ * @author Ezequiel
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        int totalAlunos = 5;
+        
+        Scanner scan = new Scanner(System.in);
+        
+        while(totalAlunos > 0){
+            
+            System.out.println("\nInforme o nome do aluno: ");
+            String nomeAluno = scan.nextLine();           
+            
+            System.out.println("Informe a idade do alulo: ");
+            int idadeAluno = Integer.parseInt(scan.nextLine());
+            
+            System.out.println("O nome do aluno é: " + nomeAluno 
+                    + " e sua idade é: " + idadeAluno + " anos");
+            
+            totalAlunos--;        
+        }
+    }
+    
+}
