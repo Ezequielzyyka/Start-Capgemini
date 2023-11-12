8e6fcc9eb1cadf8314e93b8825fb1f3221f950b1
Ezequiel-Zyyka <ezequiel.braz.es@gmail.com>
2023-11-12 20:52:43 -0300

    Implementei todos os métodos e finalizei

--- ProjetosPOO/PostoCombustivelPOO/src/main/java/PostoCombustivelPOO/BombaCombustivel.java
+++ ProjetosPOO/PostoCombustivelPOO/src/main/java/PostoCombustivelPOO/BombaCombustivel.java
@@ -10,43 +10,92 @@ import java.util.Scanner;
 public class BombaCombustivel {
 
     private String tipoCombustivel;
-    private Float valorLitroCombust;
+    private float valorLitroCombust = 0.0f;
     private float qtdCombustivel;
-    
+
     public BombaCombustivel() {
-        
+
     }
-    
+
     public void abastecerPorValor() {
         Scanner scan = new Scanner(System.in);
         float valorAAbastecer;
-        
-        System.out.println("Informe o valor a ser abastecido. ");
-        valorAAbastecer = scan.nextFloat();
-        
-        if (tipoCombustivel == "Álcool") {
-            
-        } else if (tipoCombustivel == "Gasolina") {
-            
+        float qtdCombust;
+
+        if (this.valorLitroCombust <= 0) {
+            System.out.println("Combustível sem especificação de presso! "
+                    + "\nPreço atual - " + this.tipoCombustivel + " - R$ " + this.valorLitroCombust);
+
         } else {
-            
+            System.out.println("Informe o valor a ser abastecido. ");
+            valorAAbastecer = scan.nextFloat();
+
+            qtdCombust = this.qtdCombustivel - (valorAAbastecer / this.valorLitroCombust);
+
+            setQtdCombustivel(qtdCombust);
+            if (qtdCombust >= 0) {
+                System.out.println("Litros abastecidos - " + (valorAAbastecer / this.valorLitroCombust) + " litros");
+            }
         }
     }
-    
+
     public void abasterPorLitro() {
-        
+        Scanner scan = new Scanner(System.in);
+        int qtdLitrosAbastecer;
+        float qtdCombust;
+       
+        if (this.valorLitroCombust <= 0) {
+            System.out.println("Combustível sem especificação de presso! "
+                    + "\nPreço atual - " + this.tipoCombustivel + " - R$ " + this.valorLitroCombust);
+
+        } else {
+            System.out.println("Informe a quantidade de litros a ser abastecido. ");
+            qtdLitrosAbastecer = scan.nextInt();
+            
+            qtdCombust = this.qtdCombustivel - qtdLitrosAbastecer;
+            
+            setQtdCombustivel(qtdCombust);
+            if (qtdCombust >= 0) {
+                
+                
+                System.out.println("Valor a pagar R$ " + (this.valorLitroCombust * qtdLitrosAbastecer));
+            }
+        }
     }
-    
+
     public void alterarValor() {
-        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Informe o valor do combustível. ");
+        this.valorLitroCombust = scan.nextFloat();
     }
-    
+
     public void alterarCombustivel() {
+        Scanner scan = new Scanner(System.in);
+        char combustivel;
+
+        System.out.println("Informe o tipo de combustível: 'A'-Álcool, 'G'-Gasolina ou 'D'-Diesel. ");
+        combustivel = scan.next().charAt(0);
+
+        switch (combustivel) {
+            case 'á','Á','a','A' -> this.tipoCombustivel = "Álcool";
+            case 'g','G' -> this.tipoCombustivel = "Gasolina";
+            case 'd','D' -> this.tipoCombustivel = "Diesel";
+            default -> {
+                System.out.println("Opção inválida! ");
+                this.tipoCombustivel = "";
+                scan.nextLine();
+            }
+        }
         
+
     }
-    
+
     public void alterarQuantidadeCombustivel() {
-        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Informe a quantidade que deseja abastecer o reservatório da bomba. ");
+        setQtdCombustivel(scan.nextFloat() + this.qtdCombustivel);
     }
 
     public String getTipoCombustivel() {
@@ -70,7 +119,17 @@ public class BombaCombustivel {
     }
 
     public void setQtdCombustivel(float qtdCombustivel) {
-        this.qtdCombustivel = qtdCombustivel;
+
+        if (qtdCombustivel < 0) {
+            System.out.println("Bomba não contem quantidade de combustível suficiente no reservatório! \n" + this.tipoCombustivel );
+            System.out.println("Quntidade restante - " + getQtdCombustivel() + " litros. ");
+        } else if (qtdCombustivel > 10000) {
+            System.out.println("Reservatório da bomba não cabe essa quantidade de combustível! \n" 
+                    + this.tipoCombustivel + "\nLimite máximo 10000 litros! ");
+            System.out.println("Quantidade existente no reservatório - " + getQtdCombustivel() +" litros. ");
+        } else {
+            this.qtdCombustivel = qtdCombustivel;
+        }
     }
     
     
--- ProjetosPOO/PostoCombustivelPOO/src/main/java/PostoCombustivelPOO/Main.java
+++ ProjetosPOO/PostoCombustivelPOO/src/main/java/PostoCombustivelPOO/Main.java
@@ -1,9 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package PostoCombustivelPOO;
+
+import java.util.Scanner;
 
 /**
  *
@@ -11,11 +9,128 @@ package PostoCombustivelPOO;
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        BombaCombustivel combustivel = new BombaCombustivel();
+        BombaCombustivel alcool = new BombaCombustivel();
+        BombaCombustivel gasolina = new BombaCombustivel();
+        BombaCombustivel diesel = new BombaCombustivel();
+
+        char desejaContinuar = 's';
+        char menu;
+        boolean verificador;
+
+        while (desejaContinuar == 's' || desejaContinuar == 'S') {
+            System.out.println("   Escolha uma opção.  ");
+            System.out.println("'A'- Abastecer por valor. ");
+            System.out.println("'B'- Abastecer por litros. ");
+            System.out.println("'C'- Reabastecer reservatório da bomba. ");
+            System.out.println("'D'- Alterar preço de combustível. ");
+            menu = scan.next().charAt(0);
+
+            switch (menu) {
+                case 'a','A','b','B','c','C','d','D' -> {
+
+                    combustivel.alterarCombustivel();
+
+                    switch (menu) {
+
+                        case 'a','A' -> {
+
+                            if ("Álcool".equals(combustivel.getTipoCombustivel())) {
+                                alcool.setTipoCombustivel("Álcool");
+                                alcool.abastecerPorValor();
+
+                            } else if ("Gasolina".equals(combustivel.getTipoCombustivel())) {
+                                gasolina.setTipoCombustivel("Gasolina");
+                                gasolina.abastecerPorValor();
+
+                            } else {
+                                if ("Diesel".equals(combustivel.getTipoCombustivel())) {
+                                    diesel.setTipoCombustivel("Diesel");
+                                    diesel.abastecerPorValor();
+                                }
+                            }
+                        }
+                        case 'b','B' -> {
+
+                            if ("Álcool".equals(combustivel.getTipoCombustivel())) {
+                                alcool.setTipoCombustivel("Álcool");
+                                alcool.abasterPorLitro();
+
+                            } else if ("Gasolina".equals(combustivel.getTipoCombustivel())) {
+                                gasolina.setTipoCombustivel("Gasolina");
+                                gasolina.abasterPorLitro();
+
+                            } else {
+                                if ("Diesel".equals(combustivel.getTipoCombustivel())) {
+                                    diesel.setTipoCombustivel("Diesel");
+                                    diesel.abasterPorLitro();
+                                }
+                            }
+
+                        }
+                        case 'c','C' -> {
+
+                            if ("Álcool".equals(combustivel.getTipoCombustivel())) {
+                                alcool.setTipoCombustivel("Álcool");
+                                alcool.alterarQuantidadeCombustivel();
+
+                            } else if ("Gasolina".equals(combustivel.getTipoCombustivel())) {
+                                gasolina.setTipoCombustivel("Gasolina");
+                                gasolina.alterarQuantidadeCombustivel();
+
+                            } else {
+                                if ("Diesel".equals(combustivel.getTipoCombustivel())) {
+                                    diesel.setTipoCombustivel("Diesel");
+                                    diesel.alterarQuantidadeCombustivel();
+                                }
+                            }
+
+                        }
+                        
+                        case 'd','D' -> {
+                            if ("Álcool".equals(combustivel.getTipoCombustivel())) {
+                                alcool.setTipoCombustivel("Álcool");
+                                alcool.alterarValor();
+
+                            } else if ("Gasolina".equals(combustivel.getTipoCombustivel())) {
+                                gasolina.setTipoCombustivel("Gasolina");
+                                gasolina.alterarValor();
+
+                            } else {
+                                if ("Diesel".equals(combustivel.getTipoCombustivel())) {
+                                    diesel.setTipoCombustivel("Diesel");
+                                    diesel.alterarValor();
+                                }
+                            }
+                            
+                        }
+
+                    }
+                }
+                default -> {
+                    System.out.println("Opção inválida! ");
+                }
+            }
+
+            do {
+                System.out.println("Deseja fazer mais algúm abastecimento? 'S'-SIM ou 'N'-FINALIZAR. ");
+                desejaContinuar = scan.next().charAt(0);
+
+                switch (desejaContinuar) {
+                    case 's','S','n','N' -> verificador = true;
+                    default -> {
+                        System.out.println("Opção inválida! ");
+                        verificador = false;
+                    }
+                }
+            } while (!verificador);
+
+        }
+    }
+
 }
