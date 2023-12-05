a77860b111f3d2f7eddefad77805cb96b0609802
Ezequiel-Zyyka <ezequiel.braz.es@gmail.com>
2023-12-05 06:05:06 -0300

    Adicionei AprovacaoDisciplinaPOO

---
+++ ProjetosPOO/AprovacaoDiscipinaPOO/src/main/java/AprovacaoDiscipinaPOO/Disciplina.java
@@ -0,0 +1,26 @@
+
+package AprovacaoDiscipinaPOO;
+
+/**
+ *
+ * @author Ezequiel
+ */
+public class Disciplina {
+    
+    private int matricula;
+    private String nomeAluno;
+    private float notaProva;
+    private float notaTrabalho;
+    
+    public Disciplina () {
+        
+    }
+    
+    public void calcularMedia () {
+        
+    }
+    
+    public float calcularQtdParaFinal () {
+        return 0;
+    }
+}
---
+++ ProjetosPOO/AprovacaoDiscipinaPOO/src/main/java/AprovacaoDiscipinaPOO/Main.java
@@ -0,0 +1,19 @@
+
+package AprovacaoDiscipinaPOO;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ezequiel
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        
+        
+    }
+    
+}
