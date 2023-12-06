7142ed7daacfc029cf388fa5a6f5326523922e30
Ezequiel-Zyyka <ezequiel.braz.es@gmail.com>
2023-12-06 06:48:59 -0300

    Implementei os métodos da classe Aluno

--- ProjetosPOO/AprovacaoDiscipinaPOO/src/main/java/AprovacaoDiscipinaPOO/Aluno.java
+++ ProjetosPOO/AprovacaoDiscipinaPOO/src/main/java/AprovacaoDiscipinaPOO/Aluno.java
@@ -5,22 +5,29 @@ package AprovacaoDiscipinaPOO;
  *
  * @author Ezequiel
  */
-public class Disciplina {
+public class Aluno {
     
     private int matricula;
     private String nomeAluno;
-    private float notaProva;
+    private float notaProva1, notaProva2;
     private float notaTrabalho;
     
-    public Disciplina () {
+    public Aluno () {
         
     }
     
-    public void calcularMedia () {
-        
+    public float calcularMedia () {
+        return ((this.notaProva1 + this.notaProva2)*2.5f + this.notaTrabalho*2)/7;
     }
     
-    public float calcularQtdParaFinal () {
-        return 0;
+    // Vou entender o valor 5 como média.
+    public float calcularPontosParaFinal () {
+        float media = calcularMedia();
+        
+        if (media < 2) {
+            return 0;
+        } else {
+            return 10 - media;
+        }
     }
 }
